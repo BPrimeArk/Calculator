c39910abe5226edf78efd045d11fdc378246c068
Benjamin Arkorful <benprimeartist@gmail.com>
2025-09-19 11:21:52 +0000

    Added some functions

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const buttonContainer = document.querySelector(".buttonContainer")
+const buttonContainer = document.querySelector(".buttonContainer");
+const buttons = document.querySelectorAll(".keys");
+const display = document.querySelector(".displaySection");
 
+buttons.addEventListener('click', () => {
+  display.textContent= dataset.id;
+});
+
+function addition(x, y) {
+  return x + y
+};
+
+function subrraction(x, y) {
+  return x - y
+};
+
+function division(x, y) {
+  return x / y
+};
+
+function multiplication(x, y) {
+  return x * y
+};
+
+function operation(x, y, operator) {
+  
+}
