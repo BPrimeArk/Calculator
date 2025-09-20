ff64e3326d643aa4452f9d755dcf4df60f5cecb0
Benjamin Arkorful <benprimeartist@gmail.com>
2025-09-20 11:03:19 +0000

    learnt some stuff
    started making more functions. learning exp

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const buttonContainer = document.querySelector(".buttonContainer");
 const buttons = document.querySelectorAll(".keys");
 const display = document.querySelector("#displaySection");
+const operatorButtons = document.querySelectorAll(".key");
+let operator;
+let firstNumber;
+let secondNumber;
 
 buttons.forEach(button => {
   button.addEventListener('click', () =>{
     display.textContent += button.dataset.id;
   });
 });
+
+function getOperator(){
+  operatorButtons.forEach(operatorButton => {
+  operatorButton.addEventListener('click', () =>{
+    operator = operatorButton.dataset.id;
+  });
+})}
+
+function getNumbers() {
+  const input = display.textContent;
+  let data = input.trim();
+  let split = data.split(getOperator());
+  console.log(data);
+  console.log(split);
+  firstNumber = split[0];
+  secondNumber = split[1];
+  console.log(firstNumber);
+}
+
+
+document.querySelector('.r').addEventListener('click', getNumbers())
+
+function operation(x, y, operator) {
+  operator = getOperator();
+
+}
 
 display.style.backgroundColor
 
